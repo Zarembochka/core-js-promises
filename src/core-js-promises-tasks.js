b7f5db06b7209232136a082d8e6c82b75a9d9e16
Luba Hadunova <luba.godunova2018@gmail.com>
2024-03-20 09:25:04 +0100

    feat: add solution for getFirstPromiseResult task

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -85,8 +85,15 @@ function getFirstResolvedPromiseResult(promises) {
  * [promise3, promise6, promise2] => Promise rejected with 2
  * [promise3, promise4, promise6] => Promise rejected with 6
  */
-function getFirstPromiseResult(/* promises */) {
-  throw new Error('Not implemented');
+function getFirstPromiseResult(promise) {
+  return new Promise((resolve, reject) => {
+    promise.forEach((item) =>
+      item.then(
+        (value) => resolve(value),
+        (value) => reject(value)
+      )
+    );
+  });
 }
 
 /**
