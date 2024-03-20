7c93e3b0b78f32671da3c3aed2366d2b42ac363b
Luba Hadunova <luba.godunova2018@gmail.com>
2024-03-20 09:36:44 +0100

    feat: add solution for getAllResult task

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -132,8 +132,17 @@ function getAllOrNothing(promises) {
  * [Promise.resolve(1), Promise.resolve(2), Promise.resolve(3)] => Promise fulfilled with [1, 2, 3]
  * [Promise.resolve(1), Promise.reject(2), Promise.resolve(3)]  => Promise fulfilled with [1, null, 3]
  */
-function getAllResult(/* promises */) {
-  throw new Error('Not implemented');
+function getAllResult(promises) {
+  return new Promise((resolve) => {
+    const results = [];
+    promises.forEach((item) =>
+      item.then(
+        (value) => results.push(value),
+        () => results.push(null)
+      )
+    );
+    resolve(results);
+  });
 }
 
 /**
